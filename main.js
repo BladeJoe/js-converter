01f7a1da177782ec709d9a6795571401ffdebe20
Joe <106337992+BladeJoe@users.noreply.github.com>
2022-08-13 22:30:14 +0500

    -m

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 let img = document.querySelector(".img");
+let btn = document.querySelector(".btn");
 
 
 fetch("https://forza-api.tk/")
     .then((response) => response.json())
     .then((data) => {
-        console.log(data);
         img.src = data.image
     });
+
+
+btn.addEventListener("click", function () {
+    fetch("https://forza-api.tk/")
+        .then((response1) => response1.json())
+        .then((data) => {
+            img.src = data.image
+        });
+})
