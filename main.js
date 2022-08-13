76d963a87ef50166e0ec124c94630e5a9c825650
Joe <106337992+BladeJoe@users.noreply.github.com>
2022-08-13 22:21:33 +0500

    -m

--- main.js
+++ main.js
@@ -1,24 +1,9 @@
-var usdRate = 10870.50;
-var eurRate = 11068.25;
-var rubRate = 170.00;
-var currency = document.querySelector("#currency")
-var preconvert = document.querySelector("#preconvert");
-var postconvert = document.querySelector("#postconvert");
-var form = document.querySelector(".form");
-form.addEventListener("submit", function (evt) {
-    evt.preventDefault();
-    var inputValue = preconvert.value;
+let img = document.querySelector(".img");
 
-    switch (currency.value) {
-        case "usd":
-            postconvert.value = inputValue * usdRate;
-            break;
-        case "eur":
-            postconvert.value = inputValue * eurRate;
-            break;
-        case "rub":
-            postconvert.value = inputValue * rubRate;
-            break;
 
-    }
-});
+fetch("https://forza-api.tk/")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        img.src = data.image
+    });
